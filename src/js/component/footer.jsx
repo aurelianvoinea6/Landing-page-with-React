5670678c7ed7697711889adb5917fd779addd2c5
Janescobar92 <jancore92@gmail.com>
2020-10-20 17:58:35 +0000

    creating div footer

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import PropTypes from "prop-types";
+
+export const myFooter = () =>{
+
+    return (
+        <footer>
+            <p>CopyRight @ 2020 Your website</p>
+        </footer>
+
+    );
+}
