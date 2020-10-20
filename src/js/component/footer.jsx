d20ab43e71104d1a645176e5b764633005281efe
Janescobar92 <jancore92@gmail.com>
2020-10-20 20:15:39 +0000

    footer created

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
-import ReactDOM from "react-dom";
-
-import PropTypes from "prop-types";
-
-export const myFooter = () =>{
-
-    return (
-        <footer>
-            <p>CopyRight @ 2020 Your website</p>
-        </footer>
-
-    );
-}
+export const MyFooter = () => {
+	return (
+		<footer className="conainer-fluid d-flex justify-content-center align-items-end bg-dark text-white">
+			<p className="mb-0 mt-3">CopyRight @ 2020 Your website</p>
+		</footer>
+	);
+};
