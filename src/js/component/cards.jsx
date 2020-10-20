1bb4393b9efbabc2dfe46f24e5838942b83cc2d1
Janescobar92 <jancore92@gmail.com>
2020-10-20 17:18:36 +0000

    components card created

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+import Card from "react-bootstrap/Card";
+
+import PropTypes from "prop-types";
+
+export const MycardsSection = () => {
+	// crear array de objetos
+	let CardsContent = [
+		{
+			title: "cardtitle",
+			description: "description",
+			imgUrl:
+				"https://images.pexels.com/photos/5420786/pexels-photo-5420786.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+		},
+		{
+			title: "cardtitle",
+			description: "description",
+			imgUrl:
+				"https://images.pexels.com/photos/1447096/pexels-photo-1447096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+		},
+		{
+			title: "cardtitle",
+			description: "description",
+			imgUrl:
+				"https://images.pexels.com/photos/3993247/pexels-photo-3993247.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+		},
+		{
+			title: "cardtitle",
+			description: "description",
+			imgUrl:
+				"https://images.pexels.com/photos/3050275/pexels-photo-3050275.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+		}
+	];
+	return (
+		// crear map
+		CardsContent.map((card, i) => {
+			return (
+				<Card
+					style={{
+						width: "18rem",
+						display: "inline-block"
+					}}
+					key={i}
+					className>
+					<Card.Img variant="top" src={card.imgUrl} />
+					<Card.Body>
+						<Card.Title>{card.title}</Card.Title>
+						<Card.Text>{card.description}</Card.Text>
+					</Card.Body>
+				</Card>
+			);
+		})
+	);
+};
